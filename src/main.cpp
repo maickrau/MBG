f4beeb7a095fe2b4a7c2e6c968125e4e16c0a794
Mikko Rautiainen <m_rautiainen@hotmail.com>
2021-06-14 11:19:26 -0400

    rename --hpc to --error-masking

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
 		("w", "Window size. Must be 1 <= w <= k-30 (default: k-30)", cxxopts::value<size_t>())
 		("a,kmer-abundance", "Minimum k-mer abundance", cxxopts::value<size_t>()->default_value("1"))
 		("u,unitig-abundance", "Minimum average unitig abundance", cxxopts::value<double>()->default_value("2"))
-		("no-hpc", "Don't use homopolymer compression")
+		("error-masking", "Error masking", cxxopts::value<std::string>()->default_value("hpc"))
 		("blunt", "Output a bluntified graph without edge overlaps")
 		("include-end-kmers", "Force k-mers at read ends to be included")
 		("output-sequence-paths", "Output the paths of the input sequences to a file (.gaf)", cxxopts::value<std::string>())
@@ -30,7 +30,10 @@ int main(int argc, char** argv)
 	}
 	if (params.count("h") == 1)
 	{
-		std::cerr << options.help() << std::endl;
+		std::cerr << options.help();
+		std::cerr << "Options for --error-masking:" << std::endl;
+		std::cerr << "\tno\tNo error masking" << std::endl;
+		std::cerr << "\thpc\tMask homopolymer errors (default)" << std::endl;
 		exit(0);
 	}
 	bool paramError = false;
@@ -70,7 +73,21 @@ int main(int argc, char** argv)
 	bool blunt = false;
 	bool includeEndKmers = false;
 	if (params.count("blunt") == 1) blunt = true;
-	if (params.count("no-hpc") == 1) hpc = false;
+	if (params.count("error-masking") == 1)
+	{
+		if (params["error-masking"].as<std::string>() == "no")
+		{
+			hpc = false;
+		}
+		else if (params["error-masking"].as<std::string>() == "hpc")
+		{
+		}
+		else
+		{
+			std::cerr << "unknown parameter for --error-masking: \"" << params["error-masking"].as<std::string>() << "\"" << std::endl;
+			paramError = true;
+		}
+	}
 	if (params.count("include-end-kmers") == 1) includeEndKmers = true;
 	if (params.count("output-sequence-paths") == 1) outputSequencePaths = params["output-sequence-paths"].as<std::string>();
 
@@ -117,9 +134,9 @@ int main(int argc, char** argv)
 	std::cerr << "a=" << minCoverage << ",";
 	std::cerr << "u=" << minUnitigCoverage << ",";
 	std::cerr << "t=" << numThreads << ",";
-	std::cerr << "hpc=" << (hpc ? "yes" : "no") << ",";
+	std::cerr << "errormasking=" << (hpc ? "hpc" : "no") << std::endl;
 	std::cerr << "endkmers=" << (includeEndKmers ? "yes" : "no") << ",";
-	std::cerr << "blunt=" << (blunt ? "yes" : "no") << std::endl;
+	std::cerr << "blunt=" << (blunt ? "yes" : "no") << ",";
 
 	runMBG(inputReads, outputGraph, kmerSize, windowSize, minCoverage, minUnitigCoverage, hpc, blunt, numThreads, includeEndKmers, outputSequencePaths);
 }
