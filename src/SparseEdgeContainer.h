9da273025139d9f79573073b460e27f0e9d94540
Mikko Rautiainen <m_rautiainen@hotmail.com>
2022-11-22 12:03:27 +0200

    less memory for UnitigGraph

--- src/SparseEdgeContainer.h
+++ src/SparseEdgeContainer.h
@@ -9,11 +9,15 @@
 class SparseEdgeContainer
 {
 public:
+	SparseEdgeContainer();
 	SparseEdgeContainer(size_t size);
 	void addEdge(std::pair<size_t, bool> from, std::pair<size_t, bool> to);
 	std::vector<std::pair<size_t, bool>> operator[](std::pair<size_t, bool> index) const;
 	std::vector<std::pair<size_t, bool>> getEdges(std::pair<size_t, bool> from) const;
+	bool hasEdge(std::pair<size_t, bool> from, std::pair<size_t, bool> to) const;
 	size_t size() const;
+	void emplace_back();
+	void resize(size_t newSize);
 private:
 	uint32_t pairToInt(std::pair<size_t, bool> value) const;
 	std::pair<size_t, bool> intToPair(uint32_t value) const;
